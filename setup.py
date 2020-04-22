3c56de47af204fe2eac231652a1f2443d51dfd47
John Song <songq@otpp.com>
2020-04-22 13:44:23 -0400

    add

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pymarkowitz',
+      version='0.1.0',
+      description='pymarkowitz',
+      url='https://github.com/johnsoong216/pymarkowitz',
+      author='johnsoong216',
+      author_email='johnsoong216@hotmail.com',
+      license='MIT',
+      packages=['pymarkowitz'],
+      zip_safe=False)
