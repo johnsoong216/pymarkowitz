28188ad0939488e56fce588a4e448f204fae8d92
John Song <songq@otpp.com>
2020-04-22 13:56:51 -0400

    setup

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='pymarkowitz',
       author='johnsoong216',
       author_email='johnsoong216@hotmail.com',
       license='MIT',
-      packages=['pymarkowitz'],
+      install_requires=["numpy>=1.17.1", "pandas>=0.25.3", "pandas-datareader>=0.8.1", "sklearn>=0.0", "seaborn>=0.9.0", "plotly>=4.1.1", "matplotlib>=3.2.1"],
       zip_safe=False)
